1b407eca5d8bad0566e49f56db08005c723e3177
ananda89 <ananda.rcbu@gmail.com>
2023-06-19 12:08:09 +0600

    projects added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
+import emotion from "../../Assets/Projects/emotion.PNG";
 import editor from "../../Assets/Projects/codeEditor.png";
 import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
@@ -25,10 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Photo gallery application"
+              description="This is a photo gallery application built using web development technologies. It allows users to scroll through a collection of images and view additional information related to each photo by clicking on them. The application utilizes a modal view, which is a pop-up window that appears in the center of the screen."
+              ghLink="https://github.com/ashiquebiniqbal/gallery-app/tree/master"
+              demoLink="https://ananda89-photo-gallery-app.web.app/"
             />
           </Col>
 
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Multi counter app"
+              description="Counter App, built with React.js, offers a user-friendly interface for counting and tracking values. With features like Increase, Decrease, Reset, Maximum, and Minimum, users can easily perform counting operations and monitor their progress. This simple yet powerful app can find applications in various scenarios where counting and tracking values are essential."
+              ghLink="https://github.com/ashiquebiniqbal/multi-counter-app/tree/master"
+              demoLink="https://ananda89-counterapp.web.app/"
             />
           </Col>
 
@@ -47,10 +47,10 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="Asset Management App"
+              description="Asset Management Application is a robust full-stack asset management application built using Django, It is designed to help businesses efficiently track and manage their corporate assets. By providing a centralized platform for asset monitoring, customizable categories, user access control, and reporting features, it enhances operational efficiency, accountability, and informed decision-making. With MyApp, businesses can streamline asset management processes, mitigate risks, and optimize asset utilization."
+              ghLink="https://github.com/ashiquebiniqbal/asset-management"
+              //demoLink="https://editor.soumya-jit.tech/"              
             />
           </Col>
 
@@ -58,10 +58,10 @@ function Projects() {
             <ProjectCard
               imgPath={leaf}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Global Climate Change Data Forecasting"
+              description="The objective of this project is to develop a forecasting model that can predict future global climate change patterns based on historical data. By employing a random forest ensemble approach, we can develop a powerful forecasting model for global climate change. This project not only demonstrates the application of ensemble learning in Python but also addresses the pressing issue of climate change by providing insights into its future trends. The analysis and forecasts generated through this project can contribute to better understanding and management of global climate change phenomena."
+              ghLink="https://github.com/ashiquebiniqbal/Global-Climate-Change-Data-forecast"
+              demoLink="https://github.com/ashiquebiniqbal/Global-Climate-Change-Data-forecast/blob/main/Global_Average_Temperature.ipynb"
             />
           </Col>
 
@@ -69,10 +69,10 @@ function Projects() {
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              title="Forecasting Opening and Closing Stock Prices"
+              description="By utilizing Facebook Prophet, we can accurately forecast opening and closing stock prices. This project demonstrates the power of time series analysis and forecasting using a popular library like Prophet. The ability to predict future stock prices can assist investors, traders, and financial analysts in making informed decisions. With the insights gained from this forecasting model, stakeholders can better navigate the dynamic nature of the stock market and optimize their investment strategies."
+              ghLink="https://github.com/ashiquebiniqbal/Forecast-Opening-closing-Stock-price"
+              demoLink="https://github.com/ashiquebiniqbal/Forecast-Opening-closing-Stock-price/blob/main/forecast_the_opening_and_closing_prices_of_S%26P500.ipynb"
             />
           </Col>
 
@@ -80,10 +80,9 @@ function Projects() {
             <ProjectCard
               imgPath={emotion}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              title="Ai-powered People Counter Application: Smart Video IoT Solution"
+              description="The People Counter Application demonstrates the creation of a smart video IoT solution using Intel® hardware and software tools. By utilizing the Inference Engine from the Intel® Distribution of OpenVINO™ Toolkit, it accurately detects and counts people in video frames, providing valuable insights. With features like real-time counting, duration calculation, and data transmission to a local web server, this application showcases the potential of intelligent video analytics in various domains such as crowd management, retail analytics, and security monitoring."
+              ghLink="https://github.com/ashiquebiniqbal/People-counter-app"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
           </Col>
